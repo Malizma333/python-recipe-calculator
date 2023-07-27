31e45cb25bd0bddd685dc5a4c9c8d27de58f3b27
Malizma333 <malizma333@gmail.com>
2023-07-27 11:34:14 -0500

    build command

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 csvPath = 'csv_generation/recipes_sf.csv'
 itemsJsonPath = 'csv_generation/items.json'
 settingsJsonPath = 'settings.json'
+build = 'pyinstaller --onefile C:\Users\maliz\Documents\VSC Programs\Python Projects\python-recipe-calculator\main.py'
